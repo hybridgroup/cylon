f2e90634c595447772af339e7e6421ebe1a55a7b
deadprogram <ron@hybridgroup.com>
2013-10-22 12:33:17 -0700

    hello?

---
+++ examples/test.js
@@ -0,0 +1,15 @@
+var cylon = require("cylon");
+ 
+// Initialize the robot
+var robot = cylon.robot({
+  connection: { name: 'arduino', adaptor: 'firmata', port: '/dev/ttyACM0' },
+  device: { name: 'led', driver: 'led', pin: 13 },
+  
+  work: function() {
+    // we do our thing here
+    every(1.second(), function() { console.log("ehlo"); });
+  }
+});
+
+// start working
+robot.start();
