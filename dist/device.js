eda6f9db76c5cb85d2e06843bd905e9d914cf676
Andrew Stewart <andrew@stwrt.ca>
2013-11-04 12:16:42 -0800

    Namespace Device class

--- dist/device.js
+++ dist/device.js
@@ -9,79 +9,85 @@
 
 (function() {
   'use strict';
-  var Device, EventEmitter,
+  var EventEmitter, namespace,
     __bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; },
     __hasProp = {}.hasOwnProperty,
     __extends = function(child, parent) { for (var key in parent) { if (__hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor(); child.__super__ = parent.prototype; return child; };
 
   require('./cylon');
 
+  namespace = require('node-namespace');
+
   EventEmitter = require('events').EventEmitter;
 
-  module.exports = Device = (function(_super) {
-    __extends(Device, _super);
+  namespace('Cylon', function() {
+    return this.Device = (function(_super) {
+      __extends(Device, _super);
 
-    function Device(opts) {
-      if (opts == null) {
-        opts = {};
+      function Device(opts) {
+        if (opts == null) {
+          opts = {};
+        }
+        this.start = __bind(this.start, this);
+        this.self = this;
+        this.robot = opts.robot;
+        this.name = opts.name;
+        this.pin = opts.pin;
+        this.connection = this.determineConnection(opts.connection) || this.defaultConnection();
+        this.driver = this.requireDriver(opts);
+        proxyFunctionsToObject(this.driver.commands(), this.driver, Device);
       }
-      this.start = __bind(this.start, this);
-      this.self = this;
-      this.robot = opts.robot;
-      this.name = opts.name;
-      this.pin = opts.pin;
-      this.connection = this.determineConnection(opts.connection) || this.defaultConnection();
-      this.driver = this.requireDriver(opts);
-      proxyFunctionsToObject(this.driver.commands(), this.driver, Device);
-    }
 
-    Device.prototype.start = function(callback) {
-      var msg;
-      msg = "Starting device '" + this.name + "'";
-      if (this.pin != null) {
-        msg += " on pin " + this.pin;
-      }
-      Logger.info(msg);
-      return this.driver.start(callback);
-    };
-
-    Device.prototype.data = function() {
-      return {
-        name: this.name,
-        driver: this.driver.constructor.name || this.driver.name,
-        pin: this.pin != null ? this.pin.toString : null,
-        connection: this.connection.data(),
-        commands: this.driver.commands()
+      Device.prototype.start = function(callback) {
+        var msg;
+        msg = "Starting device '" + this.name + "'";
+        if (this.pin != null) {
+          msg += " on pin " + this.pin;
+        }
+        Logger.info(msg);
+        return this.driver.start(callback);
       };
-    };
 
-    Device.prototype.determineConnection = function(c) {
-      if (c) {
-        return this.robot.connections[c];
-      }
-    };
+      Device.prototype.data = function() {
+        return {
+          name: this.name,
+          driver: this.driver.constructor.name || this.driver.name,
+          pin: this.pin != null ? this.pin.toString : null,
+          connection: this.connection.data(),
+          commands: this.driver.commands()
+        };
+      };
 
-    Device.prototype.defaultConnection = function() {
-      var first, k, v, _ref;
-      first = 0;
-      _ref = this.robot.connections;
-      for (k in _ref) {
-        v = _ref[k];
-        first || (first = v);
-      }
-      return first;
-    };
+      Device.prototype.determineConnection = function(c) {
+        if (c) {
+          return this.robot.connections[c];
+        }
+      };
 
-    Device.prototype.requireDriver = function(opts) {
-      if (opts == null) {
-        opts = {};
-      }
-      Logger.debug("Loading driver '" + opts.driver + "'");
-      return this.robot.requireDriver(opts.driver, this.self, opts);
-    };
+      Device.prototype.defaultConnection = function() {
+        var first, k, v, _ref;
+        first = 0;
+        _ref = this.robot.connections;
+        for (k in _ref) {
+          v = _ref[k];
+          first || (first = v);
+        }
+        return first;
+      };
 
-    return Device;
+      Device.prototype.requireDriver = function(opts) {
+        if (opts == null) {
+          opts = {};
+        }
+        Logger.debug("Loading driver '" + opts.driver + "'");
+        return this.robot.requireDriver(opts.driver, this.self, opts);
+      };
 
-  })(EventEmitter);
+      return Device;
+
+    })(EventEmitter);
+  });
+
+  module.exports = Cylon.Device;
 
 }).call(this);
