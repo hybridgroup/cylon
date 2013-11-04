fb8c8acca80e12d1547b7c1766378bbdeae1246d
Andrew Stewart <andrew@stwrt.ca>
2013-11-04 12:21:36 -0800

    Namespace Port class

--- dist/port.js
+++ dist/port.js
@@ -9,47 +9,53 @@
 
 (function() {
   'use strict';
-  var Port;
+  var namespace;
 
-  module.exports = Port = (function() {
-    function Port(data) {
-      this.self = this;
-      this.isTcp = this.isSerial = this.isPortless = false;
-      this.parse(data);
-    }
+  namespace = require('node-namespace');
 
-    Port.prototype.parse = function(data) {
-      var match;
-      if (data === void 0) {
-        this.port = void 0;
-        return this.isPortless = true;
-      } else if (match = /(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}):(\d{1,5})/.exec(data)) {
-        this.port = match[2];
-        this.host = match[1];
-        return this.isTcp = true;
-      } else if (/^[0-9]{1,5}$/.exec(data)) {
-        this.port = data;
-        this.host = "localhost";
-        return this.isTcp = true;
-      } else {
-        this.port = data;
-        this.host = void 0;
-        return this.isSerial = true;
+  namespace('Cylon', function() {
+    return this.Port = (function() {
+      function Port(data) {
+        this.self = this;
+        this.isTcp = this.isSerial = this.isPortless = false;
+        this.parse(data);
       }
-    };
 
-    Port.prototype.toString = function() {
-      if (this.isPortless) {
-        return "none";
-      } else if (this.isSerial) {
-        return this.port;
-      } else {
-        return "" + this.host + ":" + this.port;
-      }
-    };
+      Port.prototype.parse = function(data) {
+        var match;
+        if (data === void 0) {
+          this.port = void 0;
+          return this.isPortless = true;
+        } else if (match = /(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}):(\d{1,5})/.exec(data)) {
+          this.port = match[2];
+          this.host = match[1];
+          return this.isTcp = true;
+        } else if (/^[0-9]{1,5}$/.exec(data)) {
+          this.port = data;
+          this.host = "localhost";
+          return this.isTcp = true;
+        } else {
+          this.port = data;
+          this.host = void 0;
+          return this.isSerial = true;
+        }
+      };
 
-    return Port;
+      Port.prototype.toString = function() {
+        if (this.isPortless) {
+          return "none";
+        } else if (this.isSerial) {
+          return this.port;
+        } else {
+          return "" + this.host + ":" + this.port;
+        }
+      };
 
-  })();
+      return Port;
+
+    })();
+  });
+
+  module.exports = Cylon.Port;
 
 }).call(this);
