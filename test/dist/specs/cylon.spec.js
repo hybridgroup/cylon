716f67514793f943a3bb3bf2fdcf0703fd0869af
Andrew Stewart <andrew@stwrt.ca>
2013-11-08 09:48:20 -0700

    Remove unnecessary tests

--- test/dist/specs/cylon.spec.js
+++ test/dist/specs/cylon.spec.js
@@ -1,38 +1,11 @@
 (function() {
   'use strict';
-  var cylon;
+  var Cylon;
 
-  cylon = source("cylon");
+  Cylon = source("cylon");
 
   describe("Cylon", function() {
-    it("standard async test", function(done) {
-      var bool;
-      bool = false;
-      bool.should.be["false"];
-      setTimeout(function() {
-        bool.should.be["false"];
-        bool = true;
-        return bool.should.be["true"];
-      }, 150);
-      return setTimeout(function() {
-        bool.should.be["true"];
-        return done();
-      }, 300);
-    });
-    it("standard sync test", function() {
-      var data, obj;
-      data = [];
-      obj = {
-        id: 5,
-        name: 'test'
-      };
-      data.should.be.empty;
-      data.push(obj);
-      data.should.have.length(1);
-      data[0].should.be.eql(obj);
-      return data[0].should.be.equal(obj);
-    });
-    return it("should create a robot");
+    return it("should create a robot", function() {});
   });
 
 }).call(this);
--- test/src/specs/cylon.spec.coffee
+++ test/src/specs/cylon.spec.coffee
@@ -1,35 +1,9 @@
 'use strict';
 
-cylon = source("cylon")
+Cylon = source("cylon")
 
 describe "Cylon", ->
-  it "standard async test", (done) ->
-    bool = false
-    bool.should.be.false
-
-    setTimeout ->
-      bool.should.be.false
-      bool = true
-      bool.should.be.true
-    , 150
-
-    setTimeout ->
-      bool.should.be.true
-      done()
-    , 300
-
-  it "standard sync test", ->
-    data = []
-    obj = id: 5, name: 'test'
-    data.should.be.empty
-    data.push obj
-    data.should.have.length 1
-    # soft equal
-    data[0].should.be.eql obj
-    # hard equal
-    data[0].should.be.equal obj
-
-  it "should create a robot" #, ->
+  it "should create a robot", ->
     #cylon.should.have.keys 'robot'
     #robot = cylon.robot(name: 'caprica six')
     #robot.name.should.be.eql 'caprica six'
