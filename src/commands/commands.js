6a2cb9c907d15cbedcbeec659f1dfdb1614a9547
Edgar O Silva <oswaldo.silva@gmail.com>
2014-01-28 11:41:55 -0600

    Removed scan command from commands list.

--- src/commands/commands.js
+++ src/commands/commands.js
@@ -1,43 +1,7 @@
-var sys = require('sys'),
-    exec = require('child_process').exec,
-    os = require('os');
+var Scan = require('./scan');
 
 var commands = {
-
-  scan: function(type) {
-    switch(os.platform()){
-    case 'linux':
-      switch(type){
-      case 'serial':
-        this._stdExec('ls /dev/tty*');
-        break;
-      case 'bluetooth':
-        this._stdExec('hcitool scan');
-        break;
-      case 'usb':
-        this._stdExec('lsusb');
-        break;
-      default:
-        sys.print('Device type not yet supported...\n')
-      }
-      break;
-    case 'darwin':
-      this._stdExec('ls /dev/tty*');
-      break;
-    default:
-      sys.print('OS not yet supported...\n')
-      break;
-    }
-  },
-
-  _stdExec: function(command){
-    exec(command, function(error, stdout, stderr){
-      sys.print(stdout);
-      if (stderr != '' && stderr != null){
-        sys.print('Some errors were found: \n' + stderr);
-      }
-    });
-  }
+  scan: Scan
 }
 
 module.exports = commands;
