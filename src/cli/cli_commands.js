73fe96ea9c53d7dde28698a69b7973abddd567b0
Edgar O Silva <oswaldo.silva@gmail.com>
2014-01-30 14:13:06 -0600

    WIP on adding upload firmata command to cylon.

--- src/cli/cli_commands.js
+++ src/cli/cli_commands.js
@@ -1,12 +1,18 @@
 var scan = require('./scan'),
     connectToSerial = require('./connect_to_serial'),
+    firmata = require('./firmata'),
     bluetooth = require('./bluetooth');
 
 var cliCommands = {
   scan: scan,
   connectToSerial: connectToSerial,
-  pair: bluetooth.pair,
-  unpair: bluetooth.unpair
+  bluetooth: {
+    pair: bluetooth.pair,
+    unpair: bluetooth.unpair
+  },
+  firmata:{
+    upload: firmata.upload
+  }
 }
 
 module.exports = cliCommands;
---
+++ src/cli/firmata.js
@@ -0,0 +1,29 @@
+var ChildProcess = require('./child_process'),
+    sys = require('sys'),
+    os = require('os');
+
+var firmata = {
+  upload: function(serialPort, hexFile) {
+    part = '-patmega328p';
+    programmer = '-carduino';
+    baudrate = '-b115200';
+    hexFile = (hexFile === null || hexFile === undefined) ? '-Uflash:w:' + __dirname + '/hex/StandardFirmata.cpp.hex:i' : hexFile;
+    switch(os.platform()){
+    case 'linux':
+      port = '-P/dev/' + serialPort;
+      ChildProcess.spawn('avrdude', [part, programmer, port, baudrate, '-D', hexFile]);
+      break;
+    case 'darwin':
+      // FIXME: FIRMATA PORT FOR MAC HERE.
+      port = '-P/dev/' + serialPort;
+      ChildProcess.spawn('avrdude', [part, programmer, port, baudrate, '-D', hexFile]);
+      sys.print('OSX upload firmata command in the works...\n')
+      break;
+    default:
+      sys.print('OS not yet supported...\n')
+      break;
+    }
+  }
+}
+
+module.exports = firmata;
