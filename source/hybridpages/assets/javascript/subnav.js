cfd997a0178ec62b3652fc97cee6de65b7a0e6f4
xixebombilla <tiliches88@gmail.com>
2014-02-12 17:46:07 -0600

    160 subnav platforms

--- source/hybridpages/assets/javascript/subnav.js
+++ source/hybridpages/assets/javascript/subnav.js
@@ -4,11 +4,11 @@ $(document).ready(function() {
     $(window).bind('scroll', function() {
       if ($(window).scrollTop() > navpos.top) {
         $('#mainsubnav').addClass('subNavfixed');
-        $('#HowToConnect, #HowToUse, #Commands, #Events, #Circuit, #Compatability,#SpheroColors').addClass('marginTopSubnav');
+        $('#HowToConnect, #HowToUse, #Commands, #Events, #Circuit, #Compatability,#SpheroColors,#Drivers,#HowToInstall').addClass('marginTopSubnav');
        }
        else {
          $('#mainsubnav').removeClass('subNavfixed');
-         $('#HowToConnect, #HowToUse, #Commands, #Events, #Circuit, #Compatability,#SpheroColors').removeClass('marginTopSubnav');
+         $('#HowToConnect, #HowToUse, #Commands, #Events, #Circuit, #Compatability,#SpheroColors,#Drivers,#HowToInstall').removeClass('marginTopSubnav');
          $("#mainsubnav a").removeClass("active");
        }
     });
