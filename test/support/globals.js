1b0cb6701dcf841228e374db234cd4a756baa8a6
Andrew Stewart <andrew@stwrt.ca>
2014-02-27 20:59:21 -0800

    Remove 'source loading:' log messages from tests
    Not really necessary now that we have actual tracebacks.

--- test/support/globals.js
+++ test/support/globals.js
@@ -23,9 +23,7 @@ global.stub = sinon.stub
 // can be used by test modules to require production modules,
 // relative to the base path (where the Gruntfile.js also lives)
 global.source = function (src) {
-  console.log('source loading: ' +  src)
   var resource = path.normalize('../../lib/' + src);
-
   return require(resource);
 };
 
