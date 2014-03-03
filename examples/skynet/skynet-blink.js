8a2b157cbcb93228213da4a8387bacfa6c9edf2b
Chris Matthieu <chris@matthieu.us>
2014-03-03 10:34:39 -0700

    skynet examples updated to reflect change in skynet.im messaging format

--- examples/skynet/skynet-blink.js
+++ examples/skynet/skynet-blink.js
@@ -10,13 +10,13 @@ Cylon.robot({
 
   work: function(my) {
     Logger.info("connected...");
-    my.skynet.on('message', function(channel, data) {
+    my.skynet.on('message', function(data) {
       Logger.info(data);
       var data = JSON.parse(data);
-      if(data.red == 'on') {
+      if(data.message.red == 'on') {
         my.led.turnOn()
       }
-      else if(data.red == 'off') {
+      else if(data.message.red == 'off') {
         my.led.turnOff()
       }
     });
