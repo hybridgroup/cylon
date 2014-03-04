f1d92f4c282a93c82cbc0e8bc8546784ddfd119d
deadprogram <ron@hybridgroup.com>
2014-03-04 08:57:18 -0800

    Add Rapiro example

---
+++ examples/rapiro/rapiro_servo.js
@@ -0,0 +1,23 @@
+var Cylon = require('../..');
+
+Cylon.robot({
+  connection: { name: 'arduino', adaptor: 'firmata', port: '/dev/ttyUSB0' },
+  devices: [{name: 'led', driver: 'led', pin: 17},
+            { name: 'servo', driver: 'servo', pin: 2, range: { min: 30, max: 150 } }],
+
+  work: function(my) {
+    my.led.turnOn();
+
+    var angle = 30;
+    var increment = 40;
+
+    every(1..seconds(), function() {
+      angle += increment;
+      my.servo.angle(angle);
+
+      console.log("Current Angle: " + (my.servo.currentAngle()));
+
+      if ((angle === 30) || (angle === 150)) { increment = -increment; }
+    });
+  }
+}).start();
