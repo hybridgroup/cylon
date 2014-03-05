4a3fdf7fec47ee0d3aa7165a6f99ac51989d7636
edgarsilva <edgar.osc@gmail.com>
2014-03-05 14:50:39 -0600

    WIP on multipoe arduino controlled by skynet.

--- examples/skynet/skynet-blink.js
+++ examples/skynet/skynet-blink.js
@@ -3,22 +3,25 @@ var Cylon = require('../..');
 Cylon.robot({
   connections: [
     { name: 'arduino', adaptor: 'firmata', port: '/dev/ttyACM0' },
-    { name: 'skynet', adaptor: 'skynet', uuid: "742401f1-87a4-11e3-834d-670dadc0ddbf", token: "xjq9h3yzhemf5hfrme8y08fh0sm50zfr" }
+    { name: 'skynet', adaptor: 'skynet',
+      uuid: "96630051-a3dc-11e3-8442-5bf31d98c912", token: "2s67o7ek98pycik98f43reqr90t6s9k9" }
   ],
 
-  device: { name: 'led', driver: 'led', pin: 13, connection: 'arduino' },
+  device: { name: 'led13', driver: 'led', pin: 13, connection: 'arduino' },
 
   work: function(my) {
-    Logger.info("connected...");
+    Logger.info("Skynet is listening...");
+
     my.skynet.on('message', function(data) {
       Logger.info(data);
       var data = JSON.parse(data);
       if(data.message.red == 'on') {
-        my.led.turnOn()
+        my.led13.turnOn()
       }
       else if(data.message.red == 'off') {
-        my.led.turnOff()
+        my.led13.turnOff()
       }
     });
+
   }
 }).start();
