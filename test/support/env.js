e27aa46f072945af0266a4e257a3f20a49812253
Andrew Stewart <andrew@stwrt.ca>
2014-05-24 10:34:09 -0700

    Move test support file into test/support dir

---
+++ test/support/env.js
@@ -0,0 +1,30 @@
+'use strict';
+
+process.env.NODE_ENV = 'test';
+
+var path = require('path');
+
+var chai = require('chai'),
+    sinon = require('sinon'),
+    sinonChai = require('sinon-chai');
+
+chai.use(sinonChai);
+
+global.chai = chai;
+global.sinon = sinon;
+
+global.should = chai.should();
+global.expect = chai.expect;
+global.assert = chai.assert;
+global.AssertionError = chai.AssertionError;
+
+global.spy = sinon.spy;
+global.stub = sinon.stub;
+
+// convenience function to require modules in lib directory
+global.source = function(module) {
+  return require(path.normalize('./../../lib/' + module));
+};
+
+var Cylon = source('cylon');
+Cylon.Logger.setup(false);
