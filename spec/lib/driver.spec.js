cf0d24f785c6207f26579bcb45f2906c98fe7c7c
Andrew Stewart <andrew@stwrt.ca>
2015-01-05 12:20:53 -0800

    Add API event publication
    This update gives Cylon support for the latest changes to the CPPP-IO
    spec.

--- spec/lib/driver.spec.js
+++ spec/lib/driver.spec.js
@@ -30,6 +30,10 @@ describe("Driver", function() {
       expect(driver.commands).to.be.eql({});
     });
 
+    it("sets @events to an empty array by default", function() {
+      expect(driver.events).to.be.eql([]);
+    });
+
     it("sets @interval to 10ms by default, or the provided value", function() {
       expect(driver.interval).to.be.eql(10);
 
@@ -40,6 +44,46 @@ describe("Driver", function() {
       });
 
       expect(driver.interval).to.be.eql(2000);
+    });
+  });
+
+  describe("#toJSON", function() {
+    var driver, json;
+
+    beforeEach(function() {
+      driver = new Driver({
+        connection: { name: "conn" },
+        name: "name",
+        port: "3000"
+      });
+
+      json = driver.toJSON();
+    });
+
+    it("returns an object", function() {
+      expect(json).to.be.a("object");
+    });
+
+    it("contains the driver's name", function() {
+      expect(json.name).to.eql("name");
+    });
+
+    it("contains the driver's constructor name", function() {
+      expect(json.driver).to.eql("Driver");
+    });
+
+    it("contains the driver's connection name", function() {
+      expect(json.connection).to.eql("conn");
+    });
+
+    it("contains the driver's commands", function() {
+      expect(json.commands).to.eql(Object.keys(driver.commands));
+    });
+
+    it("contains the driver's events, or an empty array", function() {
+      expect(json.events).to.eql([]);
+      driver.events = ["hello", "world"];
+      expect(driver.toJSON().events).to.be.eql(["hello", "world"]);
     });
   });
 
