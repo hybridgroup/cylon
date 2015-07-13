843e0a3217be863a71eaba4f8c70572c341650bb
Andrew Stewart <andrew@stwrt.ca>
2015-07-13 13:55:42 -0700

    Simplify logger
    This commit adds a simplified logging system, with two primary methods:
    
    - `Logger#log(String) :: void` (for normal logging)
    - `Logger#debug(String) :: void` (for debug logging)
    
    This helps dramatically simplify logging infrastructure, as well as
    enabling simpler toggling of log modes.
    
    The configuration values for the Logger have also been updated:
    
    - `logger :: Function` - function to log output to (both normal + debug)
    - `silent :: Boolean` - whether to log default normal messages or not (`--silent` flag)
    - `debug  :: Boolean` - whether or not to log debug output (`--debug` flag)
    
    Additionally, the following CLI flags are supported re: logging:
    
    - `--silent` disables normal logging (will override all other settings)
    - `--debug` enables debug logging
    
    Deprecation notices have been added for old log methods, with first-run
    messages to this effect.
    
    This change should have negligible impact on end-users, with minor
    inconvenience for library developers, as they need to switch their
    logging function calls over.
    
    Logging configuration is still possible via both
    `Cylon.Config.update(Object)`, and directly calling
    `Cylon.Logger.setup(Object)`.

--- spec/lib/logger.spec.js
+++ spec/lib/logger.spec.js
@@ -1,48 +1,38 @@
 "use strict";
 
 var Logger = lib("logger"),
-    Config = lib("config"),
-    NullLogger = lib("logger/null_logger");
+    Config = lib("config");
 
 describe("Logger", function() {
   afterEach(function() {
     // to be friendly to other specs
-    Config.logging = { logger: false, level: "debug" };
+    Config.logger = false;
     Logger.setup();
   });
 
   describe("#setup", function() {
     context("with no arguments", function() {
       it("sets up a BasicLogger", function() {
-        Config.logging = {};
+        Config.logger = null;
         Logger.setup();
-        expect(Logger.toString()).to.be.eql("BasicLogger");
+        expect(Logger.logger.name).to.be.eql("basiclogger");
       });
     });
 
     context("with false", function() {
       it("sets up a NullLogger", function() {
-        Config.logging = { logger: false };
+        Config.logger = false;
         Logger.setup();
-        expect(Logger.toString()).to.be.eql("NullLogger");
+        expect(Logger.logger.name).to.be.eql("nulllogger");
       });
     });
 
     context("with a custom logger", function() {
       it("uses the custom logger", function() {
-        var logger = { toString: function() { return "custom"; } };
-        Config.logging = { logger: logger };
+        function customlogger() {}
+        Config.logger = customlogger;
         Logger.setup();
-        expect(Logger.toString()).to.be.eql("custom");
-      });
-    });
-
-    context("with a custom logger, provided directly", function() {
-      it("uses the custom logger", function() {
-        var logger = { toString: function() { return "custom"; } };
-        Logger.setup({ logger: logger });
-        expect(Logger.toString()).to.be.eql("custom");
-        expect(Config.logging.logger).to.be.eql(logger);
+        expect(Logger.logger.name).to.be.eql("customlogger");
       });
     });
   });
@@ -51,107 +41,31 @@ describe("Logger", function() {
     var logger;
 
     beforeEach(function() {
-      logger = Config.logging.logger = {
-        debug: spy(),
-        info: spy(),
-        warn: spy(),
-        error: spy(),
-        fatal: spy()
-      };
-
-      Logger.setup();
+      logger = spy();
+      Logger.logger = logger;
     });
 
     describe("#debug", function() {
-      it("proxies to the Logger's #debug method", function() {
-        Logger.debug("Hello", "World");
-        expect(logger.debug).to.be.calledWith("Hello", "World");
+      it("proxies to the logger method", function() {
+        Logger.should.debug = true;
+        Logger.debug("debug message");
+        Logger.should.debug = false;
+        expect(logger).to.be.calledWith("debug message");
       });
     });
 
-    describe("#info", function() {
-      it("proxies to the Logger's #info method", function() {
-        Logger.info("Hello", "World");
-        expect(logger.info).to.be.calledWith("Hello", "World");
+    describe("#log", function() {
+      it("proxies to the logger method", function() {
+        Logger.log("log message");
+        expect(logger).to.be.calledWith("log message");
       });
-    });
-
-    describe("#warn", function() {
-      it("proxies to the Logger's #warn method", function() {
-        Logger.warn("Hello", "World");
-        expect(logger.warn).to.be.calledWith("Hello", "World");
-      });
-    });
-
-    describe("#error", function() {
-      it("proxies to the Logger's #error method", function() {
-        Logger.error("Hello", "World");
-        expect(logger.error).to.be.calledWith("Hello", "World");
-      });
-    });
-
-    describe("#fatal", function() {
-      it("proxies to the Logger's #fatal method", function() {
-        Logger.fatal("Hello", "World");
-        expect(logger.fatal).to.be.calledWith("Hello", "World");
-      });
-    });
-  });
-
-  describe("log levels", function() {
-    var logger;
-
-    beforeEach(function() {
-      logger = {
-        debug: spy(),
-        info: spy(),
-        warn: spy(),
-        error: spy(),
-        fatal: spy()
-      };
-
-      Config.logging = {
-        logger: logger,
-        level: "warn"
-      };
-
-      Logger.setup();
-    });
-
-    it("prevents logging below the specified level", function() {
-      Logger.debug("debug message");
-      Logger.info("info message");
-
-      expect(logger.debug).to.not.be.called;
-      expect(logger.info).to.not.be.called;
-    });
-
-    it("still logs levels equal/greater than the specified level", function() {
-      Logger.warn("warn message");
-      Logger.error("error message");
-      Logger.fatal("fatal message");
-
-      expect(logger.warn).to.be.calledWith("warn message");
-      expect(logger.error).to.be.calledWith("error message");
-      expect(logger.fatal).to.be.calledWith("fatal message");
-    });
-
-    it("defaults to 'info' level", function() {
-      delete Config.logging.level;
-      Logger.setup();
-
-      Logger.debug("debug message");
-      Logger.info("info message");
-
-      expect(logger.debug).to.not.be.called;
-      expect(logger.info).to.be.calledWith("info message");
     });
   });
 
   it("automatically updates if configuration changed", function() {
     var custom = spy();
-    expect(Logger.logger).to.be.eql(NullLogger);
-    Config.update({ logging: { logger: custom } });
+    expect(Logger.logger.name).to.be.eql("basiclogger");
+    Config.update({ logger: custom });
     expect(Logger.logger).to.be.eql(custom);
   });
 });
